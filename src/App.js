3fe6065994e06d3d86186731fdb841fe1b264a14
harshwandr <harshavardhanputta5@gmail.com>
2021-11-03 19:12:38 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Typography, AppBar, Toolbar, Container, ImageList, ImageListItem } from '@material-ui/core'
+import ImagesData from './Data.json'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h5">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container style={{ "marginTop": "80px", "textAlign": "center" }}>
+        <Typography variant="h3">Our latest version of Photography</Typography>
+        <Typography paragraph>Pleasure to take your photos as well!</Typography>
+      </Container>
+      <Container>
+        <ImageList rowHeight={170} cols={3}>
+          {ImagesData.map(imageObj =>
+            <ImageListItem key={imageObj.id} cols={1}>
+              <img src={imageObj.img} alt={imageObj.description} />
+            </ImageListItem>)}
+
+        </ImageList>
+
+      </Container>
+
+    </>
+  )
 }
 
-export default App;
+export default App
